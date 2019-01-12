774e13cd250b617f29674e341b408f53b8f56749
BKrajancic <benjamin.krajancic@student.uts.edu.au>
2019-01-13 10:30:03 +1100

    Update deploy.sh

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 echo "Running"
-set -e
-ssh root@168.235.90.109 -v exit
+eval "$(ssh-agent -s)" # Start ssh-agent cache
+chmod 600 .travis/id_rsa # Allow read access to the private key
+ssh-add .travis/id_rsa # Add the private key to SSH
+
+git config --global push.default matching
+git remote add deploy ssh://root@$IP:$PORT$DEPLOY_DIR
+git push deploy master
