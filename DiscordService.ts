8c4e3adb1e116df17f194075ecadac805d29c11e
BKrajancic <benjamin.krajancic@student.uts.edu.au>
2019-01-12 12:18:40 +1100

    Read from file to retrieve discord token

--- DiscordService.ts
+++ DiscordService.ts
@@ -74,8 +74,15 @@ export class DiscordService implements Service {
     };
 
     login(): InputQueueEntry {
-        console.log('\x1b[36m%s\x1b[0m', "\nDiscord will begin logging in");
-        this.client.login(this.BOT_TOKEN)
+        var fs = require('fs');
+        if (fs.existsSync("discordToken.txt")) {
+            console.log('\x1b[36m%s\x1b[0m', "\nReading token from file");
+            this.BOT_TOKEN = fs.readFileSync("discordToken.txt", "utf8");
+            console.log('\x1b[36m%s\x1b[0m', "\nDiscord will begin logging in");
+            this.client.login(this.BOT_TOKEN)
+        } else {
+            console.log('\x1b[36m%s\x1b[0m', "\nUnable to find file to read token from, discord will not login.");
+        }
         return null;
     };
 }
