e92ca9d9ca3fc2cb9e79801c02794f1ac336697f
BKrajancic <benjamin.krajancic@student.uts.edu.au>
2019-01-13 12:58:11 +1100

    fix

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,10 +4,23 @@ chmod 600 id_rsa # Allow read access to the private key
 ssh-add id_rsa # Add the private key to SSH
 
 echo -e "Host $IP\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+
+sftp root@$IP <<EOF
+    cd $DEPLOY_DIR
+    put -r bin
+EOF
+
+exit
+
+bash ".travis/deploy.sh"
+
+
 git config --global push.default matching
 git remote add deploy ssh://root@$IP$DEPLOY_DIR
 git push deploy master
 echo "Completed!"
+
+
 
 ssh root@$IP <<EOF
   cd $DEPLOY_DIR
