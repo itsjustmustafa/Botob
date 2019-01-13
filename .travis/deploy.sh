c60e60d49b1ea9b74685ca12cf058f6587dde0ea
BKrajancic <benjamin.krajancic@student.uts.edu.au>
2019-01-13 13:51:17 +1100

    Update deploy.sh

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -10,26 +10,16 @@ sftp root@$IP << !
     put -r bin
 !
 
-exit
-
-bash ".travis/deploy.sh"
-
-
 git config --global push.default matching
 git remote add deploy ssh://root@$IP$DEPLOY_DIR
 git push deploy master
 echo "Completed!"
 
-
-
 ssh root@$IP <<EOF
   cd $DEPLOY_DIR
   pkill node
-  tsc -p tsconfig.json
-  tsc -p tsconfig.json
   cd bin
   node main.js
-  exit
 EOF
 
 exit
